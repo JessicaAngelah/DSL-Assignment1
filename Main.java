4bb34084002c2b80e53ba93a92085cac066ab617
JessicaAngela <114288401+JessicaAngelah@users.noreply.github.com>
2023-02-26 01:05:08 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,29 @@
+abstract class Shape {
+    String color;
+    abstract int getArea();
+    public Shape(String color){
+        this.color = color;
+    }
+
+}
+
+class Rectangle extends Shape {
+    int width;
+    int height;
+    public Rectangle(int width, int height, String color){
+        super(color);
+        this.width = width;
+        this.height = height;
+    }
+    @Override
+    public int getArea(){
+        return this.height * this.width;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Rectangle bentuk = new Rectangle (80,50,"Black");
+        System.out.println("Area of Rectangle:" + bentuk.getArea()  );
+    }
+}
